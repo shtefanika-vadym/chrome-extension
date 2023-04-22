aabf9f3a60a17945b7f628668619eaea08439414
shtefanika-vadym <vadym.shtefanika@gmail.com>
2023-04-22 07:56:39 +0300

    Adding base message

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,1 +1,9 @@
 console.log('background')
+
+chrome.webNavigation.onCommitted.addListener((details) => {
+  if (['reload', 'link'].includes(details.transitionType)) {
+    chrome.tabs.sendMessage(details.tabId, { type: 'PARSE_PAGE' }, (response) => {
+      console.log('response, onCommitted')
+    })
+  }
+})
